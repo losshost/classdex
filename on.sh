c0e6f55f3639062169880071e686d20bb7bccc9a
losshost <63525930+losshost@users.noreply.github.com>
2020-04-26 14:03:19 +0700

    Add files via upload

---
+++ on.sh
@@ -0,0 +1,23 @@
+
+echo "╔ loss "
+sleep 0.2
+echo "╠ loss 1 "
+sleep 0.2
+echo "╠ loss 2 "
+sleep 0.2
+echo "╠ loss 3 "
+sleep 0.2
+echo "╠ Fuck PUBG MOBILE "
+sleep 0.2
+echo "╚ PUBG MOBILE STARTING"
+sleep 1
+echo "==================="
+echo "BackUp Original Libs "
+echo "==================="
+sleep 0.5
+am start -n com.pubg.krmobile/com.epicgames.ue4.SplashActivity
+echo "==================="
+echo " A C T I V A T E D "
+echo "==================="
+echo ""
+
